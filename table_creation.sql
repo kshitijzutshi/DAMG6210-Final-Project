55241d35c8705350d68e790d8c266e881952bff5
Raj Sudharshan <sudharshann05@gmail.com>
2022-08-01 21:12:31 -0700

    created and updated triggers in booking entity

--- table_creation.sql
+++ table_creation.sql
@@ -80,6 +80,89 @@ WHERE CarTierID = @CarTierID;
 RETURN @cartype;
 END;
 
+
+create FUNCTION dbo.checkPrimaryCard(@CustomerID INT)
+returns INT
+AS
+BEGIN
+	DECLARE @flag INT = 0;
+
+	if EXISTS (SELECT
+		*
+	FROM CardDetails
+	WHERE CustomerID = @CustomerID
+	AND IsPrimary = 1)
+BEGIN
+SET @flag = 1
+	end
+	return @flag
+END
+
+
+CREATE FUNCTION dbo.checkavailability(@CarID int)
+returns BIT
+AS
+BEGIN
+	declare @bookid INT;
+	DECLARE @starttime DATETIME;
+	DECLARE @endtime DATETIME;
+	DECLARE @isavailable BIT;
+	DECLARE @status VARCHAR;
+
+SELECT
+	@bookid = BookingID
+FROM Bookings
+WHERE CarID = @carid
+
+SELECT
+	@starttime = BookingStartTime
+FROM Bookings
+WHERE BookingID = @bookid
+
+SELECT
+	@endtime = BookingEndTime
+FROM Bookings
+WHERE BookingID = @bookid
+
+SELECT
+	@status = Status
+FROM Bookings
+WHERE BookingID = @bookid
+
+IF CURRENT_TIMESTAMP BETWEEN @starttime AND @endtime
+BEGIN
+SET @isavailable = 0
+	end
+
+	ELSE
+	if @status in ('InProgress', 'Booked')
+	begin
+SET @isavailable = 0
+	end
+
+	RETURN @isavailable
+END
+
+CREATE FUNCTION dbo.calculateBillingAmount(@PaymentID INT)
+returns DECIMAL
+AS
+BEGIN
+	DECLARE @sum DECIMAL = 0;
+	DECLARE @bookid INT;
+
+SELECT
+	@bookid = BookingID
+FROM Bookings
+WHERE @PaymentID = @PaymentID
+
+SELECT
+	@sum = RentalAmount + Penalty
+FROM Bookings
+WHERE BookingID = @bookid
+
+RETURN @sum
+END
+
 -- TRIGGERS
 CREATE TRIGGER dbo.SET_UPDATEDATE ON UserAuth AFTER UPDATE AS BEGIN
 UPDATE UserAuth
@@ -87,14 +170,26 @@ SET UpdatedAt = CURRENT_TIMESTAMP;
 END;
 
 
-CREATE TRIGGER dbo.ResetCarMaintenance
+-- change it to insert as maintenanceid is auto inc primary key (should be composite key)
+CREATE TRIGGER dbo.ResetCarMaintenance -- edit - include carid, only after update on service date
 ON CarMaintenance
-AFTER UPDATE
+AFTER INSERT
 AS
 BEGIN
+DECLARE @carid INT;
+DECLARE @serDate DATETIME;
+
+
+SELECT
+	@carid = c.CarID
+FROM Car c
+INNER JOIN inserted i
+	ON c.CarID = i.CarID;
+
 UPDATE CarMaintenance
-SET ServiceDate = DATEADD(YEAR, 1, ServiceDate)
+SET DueDate = DATEADD(YEAR, 1, ServiceDate)
    ,DueMiles = 1500
+WHERE CarID = @carid
 END
 
 CREATE TRIGGER dbo.UpdateClosedTime
@@ -123,9 +218,9 @@ WHERE ServiceID = @serid
 END;
 END;
 
-ALTER TRIGGER dbo.UpdateMeterRating
+ALTER TRIGGER dbo.UpdateBookingsTable
 ON dbo.Bookings
-AFTER UPDATE
+AFTER INSERT, UPDATE
 AS
 BEGIN
 DECLARE @status VARCHAR;
@@ -134,12 +229,23 @@ DECLARE @meterend INT;
 DECLARE @meterstart INT;
 DECLARE @carid INT;
 DECLARE @meterrating INT;
+DECLARE @pricepermile DECIMAL;
+DECLARE @maxmilesperhr INT = 30;
+DECLARE @cartierid INT;
+DECLARE @bookingend DATETIME;
+DECLARE @bookingstart DATETIME;
+DECLARE @totalmiles INT;
+DECLARE @allocatedmiles INT;
+DECLARE @extratime DATETIME;
+DECLARE @extratimepenalty DECIMAL;
+DECLARE @ActualEndTime DATETIME;
+DECLARE @penalty DECIMAL = 0;
 
 SELECT
 	@bookid = b.BookingID
-FROM Bookings b
-INNER JOIN inserted i
-	ON b.BookingID = i.BookingID;
+FROM INSERTED i
+FULL JOIN deleted d
+	ON i.BookingID = d.BookingID;
 
 SELECT
 	@status = Status
@@ -162,15 +268,69 @@ FROM Bookings
 WHERE BookingID = @bookid
 
 SELECT
+	@cartierid = CarTierID
+FROM Car
+WHERE CarID = @carid
+
+SELECT
 	@meterrating = MeterRating
 FROM Car
 WHERE CarID = @carid
 
-IF @status = 'Completed'
-BEGIN
+SELECT
+	@bookingend = BookingEndTime
+FROM Bookings
+WHERE BookingID = @bookid
+
+SELECT
+	@bookingstart = BookingStartTime
+FROM Bookings
+WHERE BookingID = @bookid
+
+SELECT
+	@ActualEndTime = ActualEndTime
+FROM Bookings
+WHERE BookingID = @bookid
+
+SET @totalmiles = (@meterend - @meterstart)
+SET @allocatedmiles = @maxmilesperhr * (@bookingend - @bookingstart)
+
+	IF @status = 'Completed'
+	BEGIN
+
+
 UPDATE Car
 SET MeterRating = MeterRating + (@meterend - @meterstart)
 WHERE CarID = @carid
+
+UPDATE CarMaintenance
+SET DueMiles = DueMiles - (@meterend - @meterstart)
+WHERE CarID = @carid
+
+IF @totalmiles > @allocatedmiles
+BEGIN
+SET @penalty = @penalty + (@totalmiles - @allocatedmiles) * (SELECT
+		ct.PricePerMile
+	FROM Car c
+		,CarTier ct
+	WHERE ct.CarTierID = c.CarTierID)
+			END
+		IF @ActualEndTime > @bookingend
+			BEGIN
+SET @extratime = @ActualEndTime - @bookingend
+SET @extratimepenalty = CAST(DATEPART(HOUR, @extratime) AS DECIMAL) * (SELECT
+		PricePerHour
+	FROM Car c
+		,CarTier ct
+	WHERE ct.CarTierID = c.CarTierID)
+
+SET @penalty = @penalty + @extratimepenalty
+			END
+
+UPDATE Bookings
+SET Penalty = @penalty
+FROM Bookings
+WHERE BookingID = @bookid
 END
 
 ELSE
@@ -179,6 +339,22 @@ BEGIN
 UPDATE Bookings
 SET MeterStart = @meterrating
 WHERE CarID = @carid
+
+UPDATE RentalLocation
+SET CurrentCapacity = CurrentCapacity - 1
+WHERE RentalLocationID = (SELECT
+		RentalLocationID
+	FROM Car
+	WHERE CarID = @carid)
+END
+
+IF @status = 'Booked'
+BEGIN
+UPDATE Bookings
+SET RentalAmount = CAST((DATEPART(HOUR, BookingEndTime) - DATEPART(HOUR, BookingStartTime)) AS DECIMAL) * ct.PricePerHour
+FROM Bookings b, Car c, CarTier ct
+WHERE b.CarID = c.CarID
+AND c.CarTierID = ct.CarTierID
 END
 END;
 
@@ -210,8 +386,6 @@ CREATE TABLE UserAuth (
    ,CreatedAt DATETIME NOT NULL
    ,UpdatedAt DATETIME
 );
-
-
 
 
 CREATE TABLE RentalLocation (
@@ -259,9 +433,10 @@ CREATE TABLE Customer (
    ,PhoneNumber VARCHAR NOT NULL
    ,DateOfBirth DATE NOT NULL
    ,LicenseNumber VARCHAR NOT NULL
-   ,LicenseExpiry DATE NOT NULL
+   ,LicenseExpiry DATE NOT NULL CHECK (LicenseExpiry > DATEADD(MONTH, 6, CURRENT_TIMESTAMP))
    ,IsVerified BIT
    ,UserId INT NOT NULL REFERENCES UserAuth (UserId)
+   ,CONSTRAINT LicenceNumberValidCheck CHECK (LicenseNumber LIKE '^[A-Z](?:\d[- ]*){14}$')
 );
 
 ALTER TABLE Customer ADD CONSTRAINT checkValidEmailCustomer CHECK (dbo.ValidateEmail(EmailID) = 1);
@@ -276,11 +451,14 @@ CREATE TABLE CardDetails (
    ,CardNumber VARCHAR(20) NOT NULL
    ,IsPrimary BIT
    ,CustomerID INT NOT NULL REFERENCES Customer (CustomerID)
-   ,
+   ,CONSTRAINT cardExpiryCheck CHECK (ExpiryDate > DATEADD(MONTH, 6, CURRENT_TIMESTAMP))
+   ,CONSTRAINT checkprimary CHECK (dbo.checkPrimaryCard(CustomerID) = 0)
 );
+
 -- unique together on CardNumber and CustomerID 
 ALTER TABLE dbo.CardDetails
 ADD CONSTRAINT uq_cardNo_CustID UNIQUE (CardNumber, CustomerID);
+
 
 CREATE TABLE Payment (
 	PaymentID INT IDENTITY NOT NULL PRIMARY KEY
@@ -313,24 +491,24 @@ CREATE TABLE Car (
    ,SeatCapacity INT
    ,InsuranceStatus VARCHAR NOT NULL CHECK ([InsuranceStatus] IN ('ACTIVE', 'EXPIRED'))
    ,CarType AS dbo.getCarType(CarTierID)
-   ,isAvailable BIT
+   ,isAvailable AS dbo.checkavailability(CarID)
    ,RegistrationNumber VARCHAR NOT NULL
    ,DisableFriendly BIT
    ,RentalLocationID INT NOT NULL REFERENCES RentalLocation (RentalLocationID)
    ,MeterRating INT NOT NULL
    ,VendorID INT NOT NULL REFERENCES Vendor (VendorID)
-   ,
 );
 
 
 CREATE TABLE CarMaintenance (
 	MaintenanceID INT IDENTITY NOT NULL PRIMARY KEY
    ,ServiceDate DATETIME NOT NULL
-   ,DueDate AS DATEADD(YEAR, 1, ServiceDate)
+   ,DueDate DATETIME
    ,DueMiles INT
-   ,CarID INT
-   ,
+   ,CarID INT NOT NULL REFERENCES Car (CarID)
 );
+
+
 
 CREATE TABLE VendorTransactions (
 	VendorTransactionID INT IDENTITY NOT NULL PRIMARY KEY
@@ -349,14 +527,43 @@ CREATE TABLE Bookings (
    ,BookingEndTime DATETIME NOT NULL
    ,MeterStart INT
    ,MeterEnd INT
-   ,RentalAmount DECIMAL(8, 2)
+   ,RentalAmount AS dbo.calculateRentalAmount(CarID)
    ,Penalty DECIMAL(8, 2)
-   ,PaymentId INT NOT NULL REFERENCES Payment (PaymentId)
+   ,PaymentId INT REFERENCES Payment (PaymentId)
    ,CarID INT NOT NULL REFERENCES Car (CarID)
    ,ActualStartTime DATETIME
    ,ActualEndTime DATETIME
    ,BookingRating INTEGER
 );
+
+CREATE FUNCTION dbo.calculateRentalAmount (@CarID INT)
+RETURNS DECIMAL
+AS
+BEGIN
+DECLARE @sum INT = 0;
+DECLARE @maxmilesperhr INT = 30;
+DECLARE @cartierid INT;
+DECLARE @status VARCHAR;
+
+SELECT
+	@cartierid = CarTierID
+FROM Car
+WHERE CarID = @carid
+
+SELECT
+	@status = Status
+FROM Bookings
+WHERE CarID = @carid
+
+IF @status = 'Booked'
+BEGIN
+UPDATE Bookings
+SET RentalAmount = CAST((DATEPART(HOUR, BookingEndTime) - DATEPART(HOUR, BookingStartTime)) AS DECIMAL) * ct.PricePerHour
+FROM Bookings b, Car c, CarTier ct
+WHERE b.CarID = c.CarID
+AND c.CarTierID = ct.CarTierID
+END
+END
 
 CREATE TABLE CustomerService (
 	ServiceID INT IDENTITY NOT NULL PRIMARY KEY
@@ -374,20 +581,38 @@ CREATE TABLE CustomerService (
 -- TODO:
 
 -- USERAUTH
--- use HASHBYTES function to insert data  (https://www.mytecbits.com/microsoft/sql-server/sha-2-hashing)
+-- use HASHBYTES function to insert data  (https://www.mytecbits.com/microsoft/sql-server/sha-2-hashing) - completed
 
 -- CARDETAILS
 -- unique together on CardNumber and CustomerID - completed
-
---PAYMENT
--- computed column to calculate total amount
+-- cvv - encrypt - during insert
 
 --CAR
---make this a computed column from Cartier - completed
---update meterrating -> meterrating + (meterend- meterstart) - completed
+-- make this a ercomputed column from Cartier - completed
+-- update meterrating -> metrating + (meterend- meterstart) - completed
+-- availability - complete
+
+-- Renatl Location
+-- current capacity - completed
+
 
 -- BOOKINGS
 -- trigger when inprogrss, get meterrating - completed
--- rentalamount - computed column
--- reduce miles for carmaintenance entity
--- create function -- actualendtime - bookingendtime or meterend - metertart * pricepermile 
+-- reduce miles for carmaintenance entity - completed
+-- rentalamount - computed column - completed
+-- penalty - create function -- actualendtime - bookingendtime * priceperhour or meterend - metertart * pricepermile - completed
+-- create trigger to create paymentid - cancelled
+
+-- Payment
+-- Calculate Billing amount - completed
+
+-- CArd Details
+-- isprimary - unique for custid - completed
+-- expirydate constarint should be more than 6 months - completed
+
+-- Customer
+-- License expiry - completed
+-- isverified to false - function
+
+-- Customer Membership
+-- isactive- false if enddate > curr date
